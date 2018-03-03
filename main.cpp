4edf5ad8c3fb694624d1895a1f0d07c7788f48be
Sean <jesky.sean@outlook.com>
2018-03-02 22:56:49 -0700

    Update main.cpp
    PERSONAL NOTE: I just realized a major mistake while lying in bed... something just felt wrong so my brain fixed it for me, after this i need to check under the hood in GDB to see how different the source code is for a member function or function is any different in ASM

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
-#include <cmath>
-
+/*
+Practicing how to write and use... and well basically just understand 
+a class with member functions, and how they work, and how they are used 
+and implemnted; then realized that hey, there is an easy way to do a swap.
+"self-taught is more fun" - im still learning myself but C++ feels like ill be 
+able to write anything when i get better at it. My favorite language!
+and the inventors books are supurb. he encurages KeepItSimpleStupid
+and now... so do I to myself....
+*/
 class swap
 {
 public:
         double x, y;
 swap(double, double);
 };
-swap::swap(double xrt, double yrt){ x = yrt; y = xrt; }
+swap::swap(double xrt, double yrt){ 
+        x = yrt; y = xrt; 
+}
+//can anyone do it easier, looked online? maybe it can be
+//written with more simplicity with some help
+
 /*or: 
-swap(dub xrt, dub xrt) : x(yrt), y(xrt) {};
-
+swap(x, y) : x(yrt), y(xrt) {};
 */
-
 int main()
 {
         std::cout << "Current Values: " << '\n';
-        std::cout << "X: 0.44 | Y: 0.76" << '\n';
-        swap i(0.44, 0.76);
-        std::cout << "X: " << i.x << " | "<< "Y: " << i.y << std::endl;
+        
+        double d = 0.44; double f = 0.76;
+        std::cout << "d: [" << d << "] | " << "f: [" << f << "]\n";
+        
+        swap i(d, f);
+        std::cout << "Swapped Values: " << '\n';
+        std::cout << "d: [" << i.x << "] | "<< "f: [" << i.y << "]\n";
 }
-
-
